85bfecb44a8bb7a07d6010172fed829ea1199420
Francesco Perrozzi <francescoperrozzi3@gmail.com>
2022-01-15 23:26:26 -0500

    commit

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,15 +7,15 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require("packs/bootstrap.bundle.min.js")
-require("packs/owl.carousel.min.js")
-require("packs/jquery.nice-select.min.js")
-require("packs/jquery.magnific-popup.min.js")
-require("packs/wow.min.js")
-require("packs/main.js")
-require("packs/jquery-3.6.0.min.js")
+// require("packs/bootstrap.bundle.min.js")
+// require("packs/owl.carousel.min.js")
+// require("packs/jquery.nice-select.min.js")
+// require("packs/jquery.magnific-popup.min.js")
+// require("packs/wow.min.js")
+// require("packs/main.js")
+// require("packs/jquery-3.6.0.min.js")
 
-
+//= require_tree
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
