0c894e3c9844b3adfda8a0b6319e9864bff95669
Francesco Perrozzi <francescoperrozzi3@gmail.com>
2022-01-21 20:49:46 -0500

    have to add CSS to contac us form

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,4 +1,21 @@
 class CustomersController < ApplicationController
   def index
-  end
+    @customer = Customer.new
+end
+
+def create
+    @customer = Customer.new(customer_params)
+    if @customer.save
+        cookies[:saved_lead] = true
+        redirect_to customers_path, notice: "Saved Successfully!"
+    else
+        redirect_to customers_path, notice: "Failed to save!"    
+    end
+end
+
+private
+
+def customer_params
+    params.require(:customer).permit(:firstname, :lastname, :email)
+end
 end
